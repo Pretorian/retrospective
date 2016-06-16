130b0c6f723162aefdda53bd1413cbbc48e308bb
Scott Dover <sdover@Scotts-MacBook-Pro.local>
2016-06-16 10:14:09 -0400

    [bugfix] Fix retrospective query
    This fixes a bug where retrospectives weren't being displayed for users
    who have added notes, but did not create the retrospective.
    
    NOTE: This needs to be refactored at some point to use ActiveRecord if possible

--- app/models/retrospective/retrospective_repository.rb
+++ app/models/retrospective/retrospective_repository.rb
@@ -3,11 +3,19 @@ class Retrospective::RetrospectiveRepository
     @retrospectives = []
   end
 
+  # @TODO Refactor this to use ActiveRecord
   def many_for_user(user_identity, count)
-    Retrospective::Retrospective
-      .where(user_identity: user_identity)
-      .order(created_at: :desc)
-      .limit(count)
+    sql = "
+      SELECT r.* from retrospective r
+      LEFT JOIN retrospective_note_map rnm ON rnm.retrospective_id=r.id
+      LEFT JOIN retrospective_note rn ON rn.id=rnm.note_id AND rn.user_identity= ?
+      WHERE (r.user_identity = ?  OR rn.user_identity IS NOT NULL)
+      GROUP BY r.id
+      ORDER BY created_at DESC
+      LIMIT ?
+    "
+
+    Retrospective::Retrospective.find_by_sql([sql, user_identity, user_identity, count])
   end
 
   def one_with_slug(slug)
